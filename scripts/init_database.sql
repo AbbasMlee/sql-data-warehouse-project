12a3ffe762d86d68cfe1ee9c05a4678f75ea3db4
Abbas <abmlee1399@gmail.com>
2026-07-06 06:46:36 +0400

    Clean up init_database.sql formatting
    Removed unnecessary blank line before the USE statement.

---
+++ scripts/init_database.sql
@@ -0,0 +1,35 @@
+/* 
+
+Creating Database with Schemas
+
+Script crates a new database named "DataWarehouse" after chcking if it already exists.
+If the databse exists, it's dropped and recreated. The script ceates 'bronze', 'silver', 'gold' schemas.
+
+NOTE:
+  Running the script will dropm the entire 'DataWarehouse' database if it exists. All data in the database will be permanently
+  lost. Ensure you have proper backups before running this script.
+*/
+
+
+USE master;
+GO
+
+--Drop and recreate 'DataWarehouse' database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+	ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	DROP DATABASE DataWarehouse;
+	GO
+
+USE DataWarehouse;
+GO
+
+--Create Schemas
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
