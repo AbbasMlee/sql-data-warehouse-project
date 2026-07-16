8fcdd9abc2e3355ce4d53e5c3a4855b7632a2142
Abbas <abmlee1399@gmail.com>
2026-07-16 07:02:21 +0400

    Add SQL script for quality checks on crm_prd_info
    This SQL script checks for data quality issues in the 'crm_prd_info' table, including nulls, duplicates, unwanted spaces, negative numbers, data standardization, and invalid date orders.

---
+++ scripts/silver/Checkin Quality of Silver Prd Table.sql
@@ -0,0 +1,32 @@
+-- Check for nulls or duplicates in Primary Key
+-- Expectation: No Results
+SELECT 
+prd_id,
+COUNT(*)
+FROM silver.crm_prd_info
+GROUP BY prd_id
+HAVING COUNT(*) > 1 OR prd_id IS NULL
+
+-- Check for Unwanted Spaces
+-- Expectation: No Results
+SELECT prd_nm
+FROM silver.crm_prd_info
+WHERE prd_nm != TRIM(prd_nm)
+
+-- Check for NULLs or Negative Nummbers
+-- Expectation: No Results
+SELECT prd_cost
+FROM silver.crm_prd_info
+WHERE prd_cost < 0 OR prd_cost IS NULL
+
+-- Data Standardization & Consostency
+SELECT DISTINCT prd_line
+FROM silver.crm_prd_info
+
+-- Check for Invalid Date Orders
+SELECT *
+FROM silver.crm_prd_info
+WHERE prd_end_dt < prd_start_dt
+
+SELECT *
+FROM silver.crm_prd_info
